89803280dc2e7051f7d818c9a42b55cbf05a7b2d
Aryeh Harris <harryttd@gmail.com>
2017-02-20 19:43:31 -0500

    Add favicon. Set height for particles to stop being able to keep scrolling

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,2 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'particlesjs-config.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+particlesJS.load('particles-js', 'particlesjs-config.json', function() {});
